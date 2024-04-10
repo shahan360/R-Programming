169a278927153419abccde788a336dc9e67eba3b
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-04-10 14:01:14 -0500

    committed on 10th April 2024

---
+++ R Tutorials/Packages/Packages.R
@@ -0,0 +1,20 @@
+.libPaths()
+
+library()
+
+search()
+
+install.packages("Package Name")
+ 
+# Install the package named "XML".
+install.packages("XML")
+
+install.packages(file_name_with_path, repos = NULL, type = "source")
+
+# Install the package named "XML"
+install.packages("E:/XML_3.98-1.3.zip", repos = NULL, type = "source")
+
+library("package Name", lib.loc = "path to library")
+
+# Load the package named "XML"
+install.packages("E:/XML_3.98-1.3.zip", repos = NULL, type = "source")
